3186b0cec55d8de9a85f033c14073e651ce6199a
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-03-29 11:47:28 +0900

    2021-03-29 시작

---
+++ chap01/src/sec06/ch04/IfMission3_1.java
@@ -0,0 +1,26 @@
+package sec06.ch04;
+
+public class IfMission3_1 {
+
+	public static void main(String[] args) {
+		//final int SCORE = (int)(Math.random() * 41) + 60;
+		final int SCORE = 79;
+		final int DIFF = 9;
+		System.out.println("SCORE : " + SCORE);
+
+
+		//91이상 A : 9 > 0
+		//81이상 B : 8 > 1
+		//71이상 C
+		int quo = SCORE / 10;
+		if(quo == 10) {
+			quo = DIFF;
+		}
+		String[] gradeArr = {"A", "B", "C", "D"};
+		
+		System.out.println(gradeArr[DIFF-quo]);
+		
+		
+	}
+
+}
